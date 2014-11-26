905131ed7929ea20194461b677ade13821d7c1fc
Rytis Alekna <rytis@samesystem.com>
2014-11-26 19:34:54 +0200

    Added more member types

--- full_class/SomeClass.js
+++ full_class/SomeClass.js
@@ -1,10 +1,33 @@
 var SomeClass = (function() {
   
+  ///
+  /// STATIC MEMBERS
+  ///
+  // It is a good practice to declare static members before non static
+  
   // private static variables are accessible through static and instance methods but not directly from outside
-  var privateStaticVariable = "Something";
-
+  var privateStaticVariable = "I\'m private static!";
+  
+  // Public static class methods
+  SomeClass.getPrivateStaticVariable = function() {
+    return privateStaticVariable;
+  };
+  
+  // Public static variables
+  // Have in mind that these variables can be easily changed by accident.
+  SomeClass.publicStaticVar = "I\'m static and public!";
+  
+  // It is used to declare constants in this scope
+  SomeClass.DEFAULTS_CONSTANT = "This is a default value";
+  
+  // Private static methods
+  // These methods are good for various utility functions
+  var privateStaticMethod = function() {
+    // it can access both private and public static variables and methods
+  };
+  
   // class constructor
-  function SomeClass(some_argument) {
+  function SomeClass(someArgument) {
     
     // private instance variables
     // use them only when you realy need them because they don't go 
@@ -24,18 +47,24 @@ var SomeClass = (function() {
     // Public instance variable.
     // You should declare public instance variables in constructor 
     // only when you need set or modify values based on parameters passed to constructor
-    this.publicInstanceVariable = "I\'m public!";
+    this.publicInstanceVariable = someArgument;
     
-  }
+  }  
   
-  // Static class methods
-  SomeClass.getPrivateStaticVariable = function() {
-    return privateStaticVariable;
-  };
-
   // in prototype you define or override public instance variables and methods that will common to all instances of class
   SomeClass.prototype = {
-
+    
+    // public instace variable
+    // Variables declared here is also accessible in constructor.
+    myPublicVar: "I\'m public too!",
+    
+    // Event if set variable's value in constructor, it is a good practice to declare them here
+    publicInstanceVariable: null,
+    
+    // You can set instance variables' values to static vars' values 
+    anotherInstanceVariable: SomeClass.DEFAULTS_CONSTANT,
+    thirdInstanceVariable: privateStaticVariable,
+    
     // in prototype you can access private instance variables only via getters and setters
     getSecretVariable: function() {
       return this.getInstanceVariable();
@@ -54,8 +83,12 @@ var SomeClass = (function() {
     // calling this method will generate ReferenceError that will say "privateInstanceVariable is not defined"
     getDirectHello: function() {
       return privateInstanceVariable;
+    },
+    
+    // from public instance methods you can easily access private static vars and methods
+    getPrivateStaticVar: function() {
+      return privateStaticVariable;
     }
-
 
   };
   
