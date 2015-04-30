bf3974bf0862e6db3ffeef810dcbc2cd7a6e73a6
Rytis Alekna <rytis@samesystem.com>
2015-04-30 12:54:54 +0300

    extension capabilities

--- test/classes/ExtendedClassSpec.js
+++ test/classes/ExtendedClassSpec.js
@@ -1,7 +1,38 @@
-/**
- * Created by rytis on 2015-04-24.
- */
-define(['classes/BaseClass', 'classes/ExtendedClass'], function (BaseClass, ExtendedClass) {
+define(function () {
+
+  var BaseClass = (function() {
+
+    function BaseClass () {
+      console.log('in constructor', this);
+      if (this == window) {
+        return new BaseClass();
+      }
+    }
+
+    BaseClass.prototype = {
+      doSomething: function () {
+        console.log('hey');
+      }
+    };
+
+    return BaseClass;
+
+  })();
+
+  var ExtendedClass = (function(__super) {
+
+    // constructor
+    function ExtendedClass () {
+      // calling super constructor
+      __super.apply(this, arguments);
+    }
+
+    ExtendedClass.prototype = new __super(); // assign prototype of parent class
+    ExtendedClass.constructor = ExtendedClass; // reassign constructor because it was overridden when new prototype assigned
+
+    return ExtendedClass;
+
+  })(BaseClass);
 
   describe('There should be demonstrated extensions of classes', function () {
 
@@ -9,6 +40,14 @@ define(['classes/BaseClass', 'classes/ExtendedClass'], function (BaseClass, Exte
       expect(new ExtendedClass()).toEqual(jasmine.any(BaseClass));
       // or in other words
       expect((new ExtendedClass()) instanceof BaseClass).toBe(true);
+    });
+
+  });
+
+  describe('About constructor', function() {
+
+    it('constructors should be not the same', function () {
+      expect(BaseClass.constructor).not.toEqual(ExtendedClass.constructor);
     });
 
   });
